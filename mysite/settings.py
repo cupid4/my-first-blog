0b209e699f004b4b2d7a914c9e096666d07a364c
ktkwon <>
2018-08-30 17:15:00 +0900

    중복 내용 제거

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,16 +40,6 @@ INSTALLED_APPS = [
     'blog',
 ]
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',
-]
-
 
 
 MIDDLEWARE = [
